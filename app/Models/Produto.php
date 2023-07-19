78e2492f2604abee8cdef8d540a6e603a5c843b3
adalnetos <adalberto.nunes@ufms.br>
2023-07-18 22:34:03 -0300

    db_crudlaravel

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Produtos extends Model
+class Produto extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['decricao', 'qtd', 'precoUnitatio', 'precoVenda'];
 }
